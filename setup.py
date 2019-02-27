6d51a72e9aa59e1af88a727827501c1043ac429c
Jeff <jeffyjefflabs@users.noreply.github.com>
2019-02-26 21:25:52 -0700

    Let's try this pypi-friendly README thing
    From https://packaging.python.org/guides/making-a-pypi-friendly-readme/

--- setup.py
+++ setup.py
@@ -17,10 +17,18 @@
 
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='jupyter-nbgallery',
     version='0.2.5',
     description='Jupyter extensions to add nbgallery integration',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/nbgallery/nbgallery-extensions',
     author='Ryan Festag',
     author_email='rfestag@gmail.com',
